b48d8d7f33ffbb9e2b57ec73079c7e3e8d8fb6e9
Anselm Brehme <development.anselm@breh.me>
2022-10-16 23:03:39 +0200

    annotations are not needed at runtime

--- src/main/java/org/cubeengine/processor/Module.java
+++ src/main/java/org/cubeengine/processor/Module.java
@@ -22,7 +22,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface Module {
     Dependency[] dependencies() default {};
